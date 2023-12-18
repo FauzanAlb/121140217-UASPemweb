61d683c180fd1fdb78bc7b1e2153bf54c933d252
Fauzan alfa a _ 121140217 <148693694+FauzanAlb@users.noreply.github.com>
2023-12-18 22:46:09 +0700

    Update UAS_pemweb.sql

--- UAS_pemweb/database/UAS_pemweb.sql
+++ UAS_pemweb/database/UAS_pemweb.sql
@@ -11,7 +11,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
+CREATE DATABASE uas_pemweb;
+USE uas_pemweb;
 --
 -- Table structure for table `tuser`
 --
